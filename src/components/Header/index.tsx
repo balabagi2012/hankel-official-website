df0369900f463b4f92da27078386ca759d2c10da
LeoZhang <balabagi2012@gmail.com>
2023-11-04 11:40:39 +0800

    feat: basic header UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,52 +3,57 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="relative mx-[80px] mt-[40px] flex flex-row items-center">
-      <div className="flex items-center mr-auto">
-        <Image src="/logo.svg" alt="hankel logo" width="62" height="77"></Image>
-        <div className="text-white ml-[12px] text-lg">
-          Hankel International Academy
-        </div>
+    <header className="fixed start-0 w-full h-[80px] px-[80px] flex flex-row items-center bg-white">
+      <div className="flex items-center mr-auto flex-1">
+        <Image
+          src="/logo.svg"
+          alt="hankel logo"
+          width="200"
+          height="42"
+        ></Image>
       </div>
       <div className="flex flex-row  items-center gap-x-[56px]">
-        <Link
-          href="/"
-          className="text-base text-white"
-          rel="noopener noreferrer"
-        >
-          <div className="text-base text-white">Home</div>
+        <Link href="/" className="text-base" rel="noopener noreferrer">
+          <div className="text-base">Home</div>
         </Link>
-        <Link
-          href="/about"
-          className="text-base text-white"
-          rel="noopener noreferrer"
-        >
-          <div className="text-base text-white">About</div>
+        <Link href="/about" className="text-base" rel="noopener noreferrer">
+          <div className="text-base">About</div>
         </Link>
-        <Link
-          href="/dayCare"
-          className="text-base text-white"
-          rel="noopener noreferrer"
-        >
-          <div className="text-base text-white">Day Care</div>
+        <Link href="/dayCare" className="text-base" rel="noopener noreferrer">
+          <div className="text-base">Day Care</div>
         </Link>
         <Link
           href="/elementary"
-          className="text-base text-white"
+          className="text-base"
           rel="noopener noreferrer"
         >
-          <div className="text-base text-white">Elementary</div>
+          <div className="text-base">Elementary</div>
         </Link>
         <Link
           href="/kindergarten"
-          className="text-base text-white"
+          className="text-base"
           rel="noopener noreferrer"
         >
-          <div className="text-base text-white">Kindergarten</div>
+          <div className="text-base">Kindergarten</div>
         </Link>
         <Link href="/middleSchool" className="" rel="noopener noreferrer">
-          <div className="text-base text-white">Middle School</div>
+          <div className="text-base">Middle School</div>
         </Link>
+      </div>
+      <div className="ml-[80px] flex flex-row items-center">
+        <Image
+          src="/icons/LanguageOutlined.svg"
+          alt="hankel language"
+          width="24"
+          height="24"
+        ></Image>
+        <div className="ml-1 mr-2 font-bold">EN</div>
+        <Image
+          src="/icons/ChevronRightFilled.svg"
+          alt="hankel chevron right"
+          width="24"
+          height="24"
+        ></Image>
       </div>
     </header>
   );
