1a1efe465f0c9aa3614781d77b0632fc017d6d12
LeoZhang <balabagi2012@gmail.com>
2023-11-19 21:46:14 +0800

    feat: init all section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Banner";
 import { Metadata } from "next";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Hankel",
@@ -20,6 +21,63 @@ export default function Home() {
         subtitle={subtitle}
         description={description}
       ></Banner>
+      <section className="text-center">
+        <div className="font-bold">Our Program</div>
+        <div className="flex-row flex justify-center">
+          <div>Hankel Kindergarten</div>
+          <div>Hankel Elementary</div>
+          <div>Hankel Middle School</div>
+          <div>Hankel Day Care</div>
+        </div>
+        <div>
+          <div className="font-bold">Hankel Kindergarten</div>
+          <div>
+            Hankel International Academy&#39;s Kindergarten provides a nurturing
+            environment for young minds to flourish. Our experienced educators
+            prioritize hands-on learning, creativity, and critical thinking,
+            laying a strong educational foundation. With a focus on exploration
+            and discovery, children grow with confidence and joy, supported by a
+            welcoming and inclusive atmosphere. Parents can trust their
+            child&#39;s early education in our caring hands.
+          </div>
+          <div className="flex flex-row flex-1 justify-center">
+            <Image
+              src="/icons/InstagramBlue.svg"
+              alt="hankel Instagram"
+              width="24"
+              height="24"
+              className="mr-[24px]"
+            ></Image>
+            <Image
+              src="/icons/FacebookBlue.svg"
+              alt="hankel Facebook"
+              width="24"
+              height="24"
+              className="mr-[24px]"
+            ></Image>
+            <Image
+              src="/icons/YoutubeBlue.svg"
+              alt="hankel Youtube"
+              width="24"
+              height="24"
+            ></Image>
+          </div>
+          <div>Learn More</div>
+        </div>
+      </section>
+      <section>
+        <Banner
+          size="small"
+          src="/subBanners/teach.png"
+          title="“Education is not the filling of a pail, but the lighting of a fire.”"
+        ></Banner>
+      </section>
+      <section>
+        <div>Latest News</div>
+      </section>
+      <section>
+        <div>Contact Us</div>
+      </section>
     </main>
   );
 }
